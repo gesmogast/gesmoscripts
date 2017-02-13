1c2a46dd8d93ffb0ab5adc80cdca180632b9b3f2
gesmogast <armagast@gmail.com>
2017-02-13 18:58:40 +0300

    Changed mariadb script a bit, added line for changing my.cnf bind-address to 0.0.0.0

--- docker-magento/db/install_mariadb.sh
+++ docker-magento/db/install_mariadb.sh
@@ -9,5 +9,9 @@ export DEBIAN_FRONTEND="noninteractive"
 sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $PASSWORD"
 apt-get install mariadb-server mariadb-client -y
+echo "Listen 0.0.0.0:3306"
+sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
 echo "Configuration of Mariadb"
 mysql -u root -p"$PASSWORD" < magentoinstall.sql
+echo "Start mariadb"
+/usr/bin/mysqld_safe
